b9c5931af7704ba39851a950baa9732801effbde
Harshitha Shetty <shettyharshitha021@gmail.com>
2025-08-26 14:35:46 +0530

    First Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div style={{backgroundImage:"url('img.png')", backgroundSize: 'cover', width: '100%', height:'500px'}}>
-
+    <div 
+      style={{
+        backgroundImage: "url('img.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '590px',
+        '@media (max-width: 768px)': {
+          height: '400px',
+        }
+      }}
+    >
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
